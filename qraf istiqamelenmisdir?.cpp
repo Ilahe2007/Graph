390cd1f22b704ee6c16d0bfc9c7cf6a5a5c070a7
İlaha Algayeva <97362483+Ilahe2007@users.noreply.github.com>
2023-04-01 01:12:25 +0400

    Create qraf istiqamelenmisdir?.cpp

---
+++ qraf istiqamelenmisdir?.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+	ll n;
+	cin>>n;
+	ll g[n+1][n+1],c=0,d=0,e=0;
+	for (ll i=1;i<=n;i++){
+		for (ll j=1;j<=n;j++){
+			cin>>g[i][j];
+			if (i==j and g[i][j]==0){
+				c++;
+			}if (i>j and g[i][j]==1){
+				d++;
+			}if(i<j and g[i][j]==1){
+				e++;
+			}
+		}
+	}if(c==n and d==e){
+		cout<<"YES"<<endl;
+	}else{
+		cout<<"NO"<<endl;
+	}
+}
